0e9a3d4f0802bda4811005df37e38b3099344286
Hectorovich <andrew.gutnik10@gmail.com>
2022-07-17 19:47:45 +0300

    Solution

--- cinema/models.py
+++ cinema/models.py
@@ -63,7 +63,10 @@ class MovieSession(models.Model):
 
 class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return str(self.created_at)
@@ -76,27 +79,42 @@ class Ticket(models.Model):
     movie_session = models.ForeignKey(
         MovieSession, on_delete=models.CASCADE, related_name="tickets"
     )
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="tickets")
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
     row = models.IntegerField()
     seat = models.IntegerField()
 
-    def clean(self):
-        for ticket_attr_value, ticket_attr_name, cinema_hall_attr_name in [
-            (self.row, "row", "count_rows"),
-            (self.seat, "seat", "count_seats_in_row"),
-        ]:
-            count_attrs = getattr(self.movie_session.cinema_hall, cinema_hall_attr_name)
-            if not (1 <= ticket_attr_value <= count_attrs):
-                raise ValidationError(
-                    {
-                        ticket_attr_name: f"{ticket_attr_name} number must be in available range: "
-                        f"(1, {cinema_hall_attr_name}): "
-                        f"(1, {count_attrs})"
-                    }
-                )
-
     def __str__(self):
-        return f"{str(self.movie_session)} (row: {self.row}, seat: {self.seat})"
+        return f"{str(self.movie_session)} " \
+               f"(row: {self.row}, seat: {self.seat})"
 
     class Meta:
         unique_together = ("movie_session", "row", "seat")
+
+    @staticmethod
+    def validate_seat_row(name, seat_row, max_seat_row, error_to_raise):
+        if not (1 <= seat_row <= max_seat_row):
+            raise error_to_raise(
+                {
+                    name: f"{name} number must be"
+                          f" in range (1, {max_seat_row})"
+                }
+            )
+
+    def clean(self):
+        Ticket.validate_seat_row(
+            "Row",
+            self.row,
+            self.movie_session.cinema_hall.rows,
+            ValidationError
+        )
+
+        Ticket.validate_seat_row(
+            "Seat",
+            self.seat,
+            self.movie_session.cinema_hall.seats_in_row,
+            ValidationError
+        )
