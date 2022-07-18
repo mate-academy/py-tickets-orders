ca2feaadeeb820b1549fb628c98265a31ea7eda3
Dorius <kornelydorius@gmail.com>
2022-07-18 18:28:10 +0300

    'Solution'

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -7,18 +7,26 @@ class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
         fields = ("id", "name")
+
+
 class ActorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Actor
         fields = ("id", "first_name", "last_name", "full_name")
+
+
 class CinemaHallSerializer(serializers.ModelSerializer):
     class Meta:
         model = CinemaHall
         fields = ("id", "name", "rows", "seats_in_row", "capacity")
+
+
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = ("id", "title", "description", "duration", "genres", "actors")
+
+
 class MovieListSerializer(MovieSerializer):
     genres = serializers.SlugRelatedField(
         many=True,
@@ -30,12 +38,17 @@ class MovieListSerializer(MovieSerializer):
         read_only=True,
         slug_field="full_name"
     )
+
+
 class MovieDetailSerializer(MovieSerializer):
     genres = GenreSerializer(many=True, read_only=True)
     actors = ActorSerializer(many=True, read_only=True)
+
     class Meta:
         model = Movie
         fields = ("id", "title", "description", "duration", "genres", "actors")
+
+
 class MovieSessionSerializer(serializers.ModelSerializer):
     class Meta:
         model = MovieSession
@@ -56,7 +69,8 @@ class MovieSessionListSerializer(MovieSessionSerializer):
 
     class Meta:
         model = MovieSession
-        fields = ("id", "show_time",
+        fields = ("id",
+                  "show_time",
                   "movie_title",
                   "cinema_hall_name",
                   "cinema_hall_capacity",
