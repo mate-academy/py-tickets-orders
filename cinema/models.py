240dbbb9a9124cad1a993e427ccc7e552c60fbad
Volodymyr Vereshchak <diakonx1985@gmail.com>
2022-10-07 17:59:01 +0300

    Solution

--- cinema/models.py
+++ cinema/models.py
@@ -51,7 +51,7 @@ class Movie(models.Model):
 
 class MovieSession(models.Model):
     show_time = models.DateTimeField()
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="movie_sessions")
     cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE)
 
     class Meta:
