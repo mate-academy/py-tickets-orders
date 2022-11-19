842b6aefda1af18cc86cd0ee9cfe5fe33ff32220
HStakhiv <gsstahiv@gmail.com>
2022-11-19 16:29:18 +0200

    Solution

--- cinema/models.py
+++ cinema/models.py
@@ -84,16 +84,19 @@ class Ticket(models.Model):
     row = models.IntegerField()
     seat = models.IntegerField()
 
-    def clean(self):
+    @staticmethod
+    def validate_seat(
+            row, seat, cinema_hall, exception
+    ):
         for ticket_attr_value, ticket_attr_name, cinema_hall_attr_name in [
-            (self.row, "row", "rows"),
-            (self.seat, "seat", "seats_in_row"),
+            (row, "row", "rows"),
+            (seat, "seat", "seats_in_row"),
         ]:
             count_attrs = getattr(
-                self.movie_session.cinema_hall, cinema_hall_attr_name
+                cinema_hall, cinema_hall_attr_name
             )
             if not (1 <= ticket_attr_value <= count_attrs):
-                raise ValidationError(
+                raise exception(
                     {
                         ticket_attr_name: f"{ticket_attr_name} "
                         f"number must be in available range: "
@@ -101,6 +104,14 @@ class Ticket(models.Model):
                         f"(1, {count_attrs})"
                     }
                 )
+
+    def clean(self):
+        self.validate_seat(
+            row=self.row,
+            seat=self.seat,
+            cinema_hall=self.movie_session.cinema_hall,
+            exception=ValidationError,
+        )
 
     def save(
         self,
