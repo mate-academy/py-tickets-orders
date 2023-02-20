6e16d30c38afb4469f4585eca436d17a22f58327
vani@linux <winfkill@gmail.com>
2023-02-20 15:15:50 +0200

    Split fixes

--- cinema/models.py
+++ cinema/models.py
@@ -113,13 +113,15 @@ class Ticket(models.Model):
         update_fields=None,
     ):
         self.full_clean()
-        super(
-            Ticket, self).save(force_insert, force_update, using, update_fields
-                               )
+        super(Ticket, self).save(
+            force_insert, force_update, using, update_fields
+        )
 
     def __str__(self) -> str:
-        return f"{str(self.movie_session)} " \
-               f"(row: {self.row}, seat: {self.seat})"
+        return (
+            f"{str(self.movie_session)} "
+            f"(row: {self.row}, seat: {self.seat})"
+        )
 
     class Meta:
         unique_together = ("movie_session", "row", "seat")
