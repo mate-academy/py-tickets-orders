7b4024960ad4e0aabb3d4000cb594f51714686e0
Mykola Zakharov <kolyazakharov94@gmail.com>
2023-03-08 01:10:04 +0200

    added serializers

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,6 +1,7 @@
+from django.db import transaction
 from rest_framework import serializers
 
-from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession
+from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order, Ticket
 
 
 class GenreSerializer(serializers.ModelSerializer):
@@ -78,3 +79,35 @@ class MovieSessionDetailSerializer(MovieSessionSerializer):
     class Meta:
         model = MovieSession
         fields = ("id", "show_time", "movie", "cinema_hall")
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    model = Ticket
+    fields = (
+        "id",
+        "movie_session",
+        "order",
+        "row",
+        "seat",
+    )
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    # tickets = TicketSerializer(
+    #     many=True,
+    #     read_only=False,
+    #     allow_empty=False
+    # )
+
+    class Meta:
+        model = Order
+        fields = ("id", "created_at", "user", "tickets")
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            tickets_data = validated_data.pop("tickets")
+            order = Order.objects.create(**validated_data)
+            for ticket_data in tickets_data:
+                Ticket.objects.create(order=order, **ticket_data)
+            return order
+
