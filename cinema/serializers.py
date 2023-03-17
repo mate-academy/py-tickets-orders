6d21908ce90be1c591956d2427ded1d55570338b
Daria Hannochenko <soloha7965@gmail.com>
2023-03-17 10:49:24 +0200

    Fix solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -136,15 +136,15 @@ class OrderSerializer(serializers.ModelSerializer):
         model = Order
         fields = ("id", "created_at", "tickets")
 
+    @transaction.atomic()
     def create(self, validated_data):
-        with transaction.atomic():
-            tickets_data = validated_data.pop("tickets")
-            order = Order.objects.create(**validated_data)
+        tickets_data = validated_data.pop("tickets")
+        order = Order.objects.create(**validated_data)
 
-            for ticket_data in tickets_data:
-                Ticket.objects.create(order=order, **ticket_data)
+        for ticket_data in tickets_data:
+            Ticket.objects.create(order=order, **ticket_data)
 
-            return order
+        return order
 
 
 class OrderListSerializer(OrderSerializer):
