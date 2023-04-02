91a53be7bf6e9ecc950a4dd1d46048a91d4be0e9
Zoriana Dvulit <zdvulit@ukr.net>
2023-04-02 21:34:48 +0300

    Solution after fixing problem

--- cinema/views.py
+++ cinema/views.py
@@ -74,22 +74,21 @@ class MovieViewSet(viewsets.ModelViewSet):
 
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
-    queryset = (
-        MovieSession.objects.all()
-        .select_related("movie", "cinema_hall")
-        .annotate(
-            tickets_available=F("cinema_hall__rows")
-            * F("cinema_hall__seats_in_row")
-            - Count("tickets")
-        )
-    )
     serializer_class = MovieSessionSerializer
 
     def get_queryset(self):
+        queryset = (
+            MovieSession.objects.all()
+            .select_related("movie", "cinema_hall")
+            .annotate(
+                tickets_available=F("cinema_hall__rows")
+                * F("cinema_hall__seats_in_row")
+                - Count("tickets")
+            )
+        )
+
         date = self.request.query_params.get("date")
         movie_id_str = self.request.query_params.get("movie")
-
-        queryset = self.queryset
 
         if date:
             date = datetime.strptime(date, "%Y-%m-%d").date()
