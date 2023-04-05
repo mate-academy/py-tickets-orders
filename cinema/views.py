09d3e018755ee7bf9578ca96dc1529b0f4a548de
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-04-05 14:20:50 +0300

    add order and ticket serializers

--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 
-from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession
+from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order, \
+    Ticket
 
 from cinema.serializers import (
     GenreSerializer,
@@ -11,7 +12,8 @@ from cinema.serializers import (
     MovieSessionListSerializer,
     MovieDetailSerializer,
     MovieSessionDetailSerializer,
-    MovieListSerializer,
+    MovieListSerializer, OrderSerializer, OrderListSerializer,
+    TicketSerializer, TicketListSerializer,
 )
 
 
@@ -56,3 +58,37 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
             return MovieSessionDetailSerializer
 
         return MovieSessionSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return OrderListSerializer
+
+        # if self.action == "retrieve":
+        #     return MovieDetailSerializer
+
+        return OrderSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return TicketListSerializer
+
+        # if self.action == "retrieve":
+        #     return MovieDetailSerializer
+
+        return TicketSerializer
