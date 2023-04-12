3e2b2dec1a3e1cc7d877b7eda0c51841c96c0d48
Roman Senyshyn <senjaskorp@gmail.com>
2023-04-12 18:55:33 +0300

    Swapped transaction.atomic(): to @transaction.atomic

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -159,13 +159,13 @@ class OrderSerializer(serializers.ModelSerializer):
         model = Order
         fields = ("tickets",)
 
+    @transaction.atomic
     def create(self, validated_data):
-        with transaction.atomic():
-            tickets_data = validated_data.pop("tickets")
-            order = Order.objects.create(**validated_data)
-            for ticket_data in tickets_data:
-                Ticket.objects.create(order=order, **ticket_data)
-            return order
+        tickets_data = validated_data.pop("tickets")
+        order = Order.objects.create(**validated_data)
+        for ticket_data in tickets_data:
+            Ticket.objects.create(order=order, **ticket_data)
+        return order
 
 
 class OrderListSerializer(OrderSerializer):
