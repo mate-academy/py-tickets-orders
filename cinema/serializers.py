2a8977205c70fdc08a01cd3c81d0e74c0350d4e9
Taras Panasiuk <107722931+tarasavage@users.noreply.github.com>
2023-04-22 22:45:13 +0300

    changes

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -126,22 +126,22 @@ class TicketListSerializer(TicketSerializer):
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    tickets = TicketSerializer(many=True, read_only=False, allow_empty=False)
+    tickets = TicketSerializer(many=True, allow_empty=False)
 
     class Meta:
         model = Order
         fields = ("id", "tickets", "created_at")
 
+    @transaction.atomic
     def create(self, validated_data):
-        with transaction.atomic():
-            tickets_data = validated_data.pop("tickets")
-            order = Order.objects.create(**validated_data)
+        tickets_data = validated_data.pop("tickets")
+        order = Order.objects.create(**validated_data)
 
-            for ticket_data in tickets_data:
-                Ticket.objects.create(order=order, **ticket_data)
+        for ticket_data in tickets_data:
+            Ticket.objects.create(order=order, **ticket_data)
 
-            return order
+        return order
 
 
 class OrderListSerializer(OrderSerializer):
-    tickets = TicketListSerializer(many=True, read_only=False)
+    tickets = TicketListSerializer(many=True)
