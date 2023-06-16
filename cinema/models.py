bf884a29adc8242db17738cfc962c4ce0d0cc7bf
Vitalii Shulepa <vitaliy.shulep@gmail.com>
2023-06-16 11:36:40 +0300

    Solution

--- cinema/models.py
+++ cinema/models.py
@@ -51,8 +51,16 @@ class Movie(models.Model):
 
 class MovieSession(models.Model):
     show_time = models.DateTimeField()
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE)
+    movie = models.ForeignKey(
+        Movie,
+        on_delete=models.CASCADE,
+        related_name="movies"
+    )
+    cinema_hall = models.ForeignKey(
+        CinemaHall,
+        on_delete=models.CASCADE,
+        related_name="cinema_halls"
+    )
 
     class Meta:
         ordering = ["-show_time"]
@@ -121,3 +129,4 @@ class Ticket(models.Model):
 
     class Meta:
         unique_together = ("movie_session", "row", "seat")
+        ordering = ("seat",)
