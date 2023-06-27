e5f2adc95c7a40bf3614a45a15717510c93c8348
Ivan <ivan.korshunov.1997@gmail.com>
2023-06-27 21:00:00 +0300

    solution

--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,9 @@
+from django.db.models import Count, F
+from django.utils.dateparse import parse_date
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
-from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession
+from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order
 
 from cinema.serializers import (
     GenreSerializer,
@@ -12,7 +15,38 @@ from cinema.serializers import (
     MovieDetailSerializer,
     MovieSessionDetailSerializer,
     MovieListSerializer,
+    OrderSerializer,
+    OrderListSerializer,
 )
+
+
+class OrderPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = "page_size"
+    max_page_size = 10
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    pagination_class = OrderPagination
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return OrderListSerializer
+        return OrderSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset.filter(user=self.request.user)
+        if self.action == "list":
+            queryset = queryset.prefetch_related(
+                "tickets__movie_session__cinema_hall",
+                "tickets__movie_session__movie"
+            )
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
 class GenreViewSet(viewsets.ModelViewSet):
@@ -43,9 +77,42 @@ class MovieViewSet(viewsets.ModelViewSet):
 
         return MovieSerializer
 
+    @staticmethod
+    def params_to_ints(qs):
+        """Converts a list of string IDs into a list of integers"""
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_queryset(self):
+        """
+        Filtering by actor, genre IDs,
+        and title has been implemented here.
+        """
+        queryset = self.queryset
+
+        actors_ids = self.request.query_params.get("actors")
+        if actors_ids:
+            actors_ids = self.params_to_ints(actors_ids)
+            queryset = queryset.filter(actors__id__in=actors_ids)
+
+        genres_ids = self.request.query_params.get("genres")
+        if genres_ids:
+            genres_ids = self.params_to_ints(genres_ids)
+            queryset = queryset.filter(genres__id__in=genres_ids)
+
+        title = self.request.query_params.get("title")
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        return queryset.distinct()
+
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
-    queryset = MovieSession.objects.all()
+    queryset = (
+        MovieSession.objects.select_related("cinema_hall", "movie")
+        .prefetch_related(
+            "movie__actors", "movie__genres", "cinema_hall", "tickets"
+        )
+    )
     serializer_class = MovieSessionSerializer
 
     def get_serializer_class(self):
@@ -56,3 +123,34 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
             return MovieSessionDetailSerializer
 
         return MovieSessionSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        if self.action == "list":
+            queryset = (
+                queryset
+                .select_related("cinema_hall", "movie")
+                .annotate(
+                    tickets_available=F("cinema_hall__rows")
+                    * F("cinema_hall__seats_in_row")
+                    - Count("tickets")
+                )
+            )
+
+        date = self.request.query_params.get("date")
+        if date:
+            date = parse_date(date)
+            queryset = queryset.filter(show_time__date=date)
+
+        movie_ids = self.request.query_params.get("movie")
+        if movie_ids:
+            movie_ids = params_to_ints(movie_ids)
+            queryset = queryset.filter(movie__id__in=movie_ids)
+
+        return queryset
+
+
+def params_to_ints(qs):
+    """Converts a list of string IDs into a list of integers"""
+    return [int(str_id) for str_id in qs.split(",")]
