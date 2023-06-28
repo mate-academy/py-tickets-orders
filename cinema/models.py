0bbce2f538d350477586277ba69e5c7994ade44a
Oleksandr Yanchuk <sashajanchuck13@gmail.com>
2023-06-28 14:01:21 +0300

    Solution

--- cinema/models.py
+++ cinema/models.py
@@ -107,9 +107,12 @@ class Ticket(models.Model):
         update_fields=None,
     ):
         self.full_clean()
-        super(
-            Ticket, self).save(
-            force_insert, force_update, using, update_fields)
+        super(Ticket, self).save(
+            force_insert,
+            force_update,
+            using,
+            update_fields,
+        )
 
     def __str__(self):
         return (
