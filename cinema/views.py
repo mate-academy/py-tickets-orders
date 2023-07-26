e7e5527c76f8096fef552dc88ad83599ad10a1ed
Olena Liubynetska <olena.liuby@gmail.com>
2023-07-26 14:36:53 +0100

    views: Fixed n1 problem in OrderViewSet

--- cinema/views.py
+++ cinema/views.py
@@ -63,7 +63,13 @@ class OrderViewSet(viewsets.ModelViewSet):
     serializer_class = OrderSerializer
 
     def get_queryset(self):
-        return self.queryset.filter(user=self.request.user)
+        queryset = self.queryset.filter(user=self.request.user)
+        if self.action == "list":
+            queryset = queryset.prefetch_related(
+                "tickets__movie_session__cinema_hall",
+                "tickets__movie_session__movie"
+            )
+        return queryset
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
