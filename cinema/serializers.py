c6bd8a479bcad6adcf39f6966452483a4cf375d8
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-10 22:55:09 +0300

    create MovieSessionTicketsSerializer and add taken_places field to MovieSessionDetailSerializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -80,13 +80,20 @@ class MovieSessionListSerializer(MovieSessionSerializer):
         )
 
 
+class MovieSessionTicketsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = ("row", "seat")
+        
+
 class MovieSessionDetailSerializer(MovieSessionSerializer):
     movie = MovieListSerializer(many=False, read_only=True)
     cinema_hall = CinemaHallSerializer(many=False, read_only=True)
-
+    taken_places = MovieSessionTicketsSerializer(source="tickets", many=True, read_only=True)
+    
     class Meta:
         model = MovieSession
-        fields = ("id", "show_time", "movie", "cinema_hall")
+        fields = ("id", "show_time", "movie", "cinema_hall", "taken_places")
 
 
 class TicketSerializer(serializers.ModelSerializer):
