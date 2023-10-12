26886c3d30300bb232c36815d44333c79ab6adb5
Marian Kovalyshyn <mariankovalishin12345@gmail.com>
2023-10-12 21:41:47 +0300

    Add order endpoint, filtering for movies and movie sessions

--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,8 @@
+from django.db.models import F, Count
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
-from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession
+from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order
 
 from cinema.serializers import (
     GenreSerializer,
@@ -12,6 +14,8 @@ from cinema.serializers import (
     MovieDetailSerializer,
     MovieSessionDetailSerializer,
     MovieListSerializer,
+    OrderListSerializer,
+    OrderCreateSerializer,
 )
 
 
@@ -34,6 +38,15 @@ class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
+    def get_queryset(self):
+        queryset = self.queryset
+
+        queryset = self._filter_by_actor(queryset)
+        queryset = self._filter_by_genre(queryset)
+        queryset = self._filter_by_title(queryset)
+
+        return queryset.distinct()
+
     def get_serializer_class(self):
         if self.action == "list":
             return MovieListSerializer
@@ -43,10 +56,51 @@ class MovieViewSet(viewsets.ModelViewSet):
 
         return MovieSerializer
 
+    def _filter_by_actor(self, queryset):
+        actors = self.request.query_params.get("actors")
+
+        if actors:
+            actors_ids = [int(actor_id) for actor_id in actors.split(",")]
+            queryset = queryset.filter(actors__id__in=actors_ids)
+
+        return queryset
+
+    def _filter_by_genre(self, queryset):
+        genres = self.request.query_params.get("genres")
+
+        if genres:
+            genres_ids = [int(genre_id) for genre_id in genres.split(",")]
+            queryset = queryset.filter(genres__id__in=genres_ids)
+
+        return queryset
+
+    def _filter_by_title(self, queryset):
+        title = self.request.query_params.get("title")
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        return queryset
+
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
     queryset = MovieSession.objects.all()
     serializer_class = MovieSessionSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        queryset = self._filter_by_movie(queryset)
+        queryset = self._filter_by_date(queryset)
+
+        if self.action == "list":
+            queryset = queryset.all().annotate(
+                tickets_available=F("cinema_hall__rows")
+                * F("cinema_hall__seats_in_row")
+                - Count("tickets")
+            )
+
+        return queryset
 
     def get_serializer_class(self):
         if self.action == "list":
@@ -56,3 +110,44 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
             return MovieSessionDetailSerializer
 
         return MovieSessionSerializer
+
+    def _filter_by_movie(self, queryset):
+        movie = self.request.query_params.get("movie")
+
+        if movie:
+            queryset = queryset.filter(movie__id=movie)
+
+        return queryset
+
+    def _filter_by_date(self, queryset):
+        date = self.request.query_params.get("date")
+
+        if date:
+            queryset = queryset.filter(show_time__date=date)
+
+        return queryset
+
+
+class OrderPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = "page_size"
+    max_page_size = 100
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.prefetch_related(
+        "tickets__movie_session__movie", "tickets__movie_session__cinema_hall"
+    )
+    serializer_class = OrderListSerializer
+    pagination_class = OrderPagination
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return OrderCreateSerializer
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
