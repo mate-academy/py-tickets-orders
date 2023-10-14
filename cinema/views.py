df6a9ef604bc9386e422e2677bd49489781c29ab
Oleh Bohatov <obohatov@gmail.com>
2023-10-14 15:00:26 +0200

    Solution with fixed mistake

--- cinema/views.py
+++ cinema/views.py
@@ -140,7 +140,9 @@ class OrderViewSet(viewsets.ModelViewSet):
         queryset = self.queryset.filter(user=self.request.user)
 
         if self.action == "list":
-            queryset = queryset.prefetch_related("tickets__movie_session__cinema_hall")
+            queryset = queryset.prefetch_related(
+                "tickets__movie_session__cinema_hall"
+            )
         return queryset
 
     def perform_create(self, serializer):
