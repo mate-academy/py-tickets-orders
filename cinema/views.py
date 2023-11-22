d258e6166e19748f71274224c415554259cd40b2
Bohdan Kolomiiets <kb907406@gmail.com>
2023-11-22 21:20:19 +0200

    Add transaction decorator, reformat annotate

--- cinema/views.py
+++ cinema/views.py
@@ -103,10 +103,12 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
             queryset = (
                 queryset
                 .select_related("movie", "cinema_hall")
-                .annotate(tickets_available=(
-                    F("cinema_hall__rows")
-                    * F("cinema_hall__seats_in_row")
-                    - Count("tickets"))
+                .annotate(
+                    tickets_available=(
+                        F("cinema_hall__rows")
+                        * F("cinema_hall__seats_in_row")
+                        - Count("tickets")
+                    )
                 )
             )
 
