f4bf15a3ca3173448f17aaf7d77cc8cb627da4cb
Soloveyk1n <swain.volt@gmail.com>
2024-02-22 16:46:12 +0200

    Solution without validate

--- cinema/models.py
+++ cinema/models.py
@@ -84,27 +84,26 @@ class Ticket(models.Model):
     row = models.IntegerField()
     seat = models.IntegerField()
 
-    def validate_seat(self, row, seat, movie_session, errors_to_raise=None):
-        for ticket_attr_value, ticket_attr_name, cinema_hall_attr_name in [
-            (row, "row", "rows"),
-            (seat, "seat", "seats_in_row"),
-            (movie_session, "movie_session")
-        ]:
-            count_attrs = getattr(
-                self.movie_session.cinema_hall, cinema_hall_attr_name
-            )
-            if not (1 <= ticket_attr_value <= count_attrs):
-                raise errors_to_raise(
-                    {
-                        ticket_attr_name: f"{ticket_attr_name} "
-                        f"number must be in available range: "
-                        f"(1, {cinema_hall_attr_name}): "
-                        f"(1, {count_attrs})"
-                    }
-                )
-
-    def clean(self):
-        Ticket.validate_seat(self.row, self.seat, self.movie_session)
+    # def validate_seat(self, row, seat, movie_session, errors_to_raise=None):
+    #     for ticket_attr_value, ticket_attr_name, cinema_hall_attr_name in [
+    #         (row, "row", "rows"),
+    #         (seat, "seat", "seats_in_row"),
+    #     ]:
+    #         count_attrs = getattr(
+    #             self.movie_session.cinema_hall, cinema_hall_attr_name
+    #         )
+    #         if not (1 <= ticket_attr_value <= count_attrs):
+    #             raise errors_to_raise(
+    #                 {
+    #                     ticket_attr_name: f"{ticket_attr_name} "
+    #                     f"number must be in available range: "
+    #                     f"(1, {cinema_hall_attr_name}): "
+    #                     f"(1, {count_attrs})"
+    #                 }
+    #             )
+    #
+    # def clean(self):
+    #     Ticket.validate_seat(self.row, self.seat, self.movie_session)
 
     def save(
         self,
