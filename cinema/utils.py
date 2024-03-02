a176ab6630a2b0d2ae8feb6adc9e8cb618de4cee
Marinel444 <380978907444m@gmail.com>
2024-03-02 18:59:20 +0200

    rename gs

--- cinema/utils.py
+++ cinema/utils.py
@@ -1,4 +1,4 @@
 class ParamsToIntsMixin:
     @staticmethod
-    def _params_to_ints(qs):
-        return [int(str_id) for str_id in qs.split(",")]
+    def _params_to_ints(query_params):
+        return [int(str_id) for str_id in query_params.split(",")]
