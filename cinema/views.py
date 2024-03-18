04b1745ca564583320cc0a3f76f68c862e1c3b34
Yevhen <evgeniy.playid@icloud.com>
2024-03-18 18:21:42 +0200

    Eliminating mistakes

--- cinema/views.py
+++ cinema/views.py
@@ -78,7 +78,8 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
         if self.action == "list":
             queryset = queryset.annotate(
                 tickets_available=(
-                        F("cinema_hall__rows") * F("cinema_hall__seats_in_row")
+                        F("cinema_hall__rows")
+                        * F("cinema_hall__seats_in_row")
                         - Count("tickets")
                 )
             )
@@ -118,7 +119,8 @@ class OrderViewSet(viewsets.ModelViewSet):
 
         if self.action == "list":
             queryset = queryset.prefetch_related(
-                "tickets__movie_session__movie", "tickets__movie_session__cinema_hall"
+                "tickets__movie_session__movie",
+                "tickets__movie_session__cinema_hall",
             )
 
         return queryset
