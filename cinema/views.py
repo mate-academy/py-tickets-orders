1aca31c4cc4feed3f9212d8119fa921c2eb0709f
Vladyslav Shepilov <shepsterya@gmail.com>
2024-04-09 22:39:47 +0300

    add filtering for MovieViewSet by actors, genres and title

--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,11 @@
+from django.db.models.functions import Concat
+from django.forms import CharField
 from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
+from rest_framework.pagination import PageNumberPagination
+from django.db.models import Q, F, CharField, Value
 
-from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession
+from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order
 
 from cinema.serializers import (
     GenreSerializer,
@@ -12,7 +17,15 @@ from cinema.serializers import (
     MovieDetailSerializer,
     MovieSessionDetailSerializer,
     MovieListSerializer,
+    OrderSerializerList,
+    OrderSerializerPost
 )
+
+
+class OrderSetPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = "page_size"
+    max_page_size = 20
 
 
 class GenreViewSet(viewsets.ModelViewSet):
@@ -43,6 +56,28 @@ class MovieViewSet(viewsets.ModelViewSet):
 
         return MovieSerializer
 
+    def get_queryset(self):
+        queryset = self.queryset
+
+        if self.action in ("list", "retrieve"):
+            queryset = queryset.prefetch_related("genres", "actors")
+
+        actors = self.request.query_params.get("actors")
+        genres = self.request.query_params.get("genres")
+        title = self.request.query_params.get("title")
+
+        if actors:
+            queryset = queryset.annotate(
+                actor_full_name=Concat('actors__first_name', Value(' '), 'actors__last_name', output_field=CharField())
+            ).filter(
+                actor_full_name__icontains=actors
+            )
+        if genres:
+            queryset = queryset.filter(genres__name=genres)
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+        return queryset
+
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
     queryset = MovieSession.objects.all()
@@ -56,3 +91,34 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
             return MovieSessionDetailSerializer
 
         return MovieSessionSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.prefetch_related(
+                "tickets__movie_session__cinema_hall",
+                "tickets__movie_session__movie"
+            )
+    serializer_class = OrderSerializerList
+    pagination_class = OrderSetPagination
+
+    def get_serializer_class(self):
+        serializer = self.serializer_class
+
+        if self.action == "create":
+            serializer = OrderSerializerPost
+
+        return serializer
+
+    def get_queryset(self):
+        queryset = self.queryset.filter(user=self.request.user)
+
+        if self.action == "list":
+            queryset = queryset.prefetch_related(
+                "tickets__movie_session__cinema_hall",
+                "tickets__movie_session__movie"
+            )
+
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
