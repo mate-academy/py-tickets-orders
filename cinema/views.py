5899a3a6c385af3f3f309b199d53fee28396309e
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-29 09:14:39 +0300

    Add pagination to OrderViewSet

--- cinema/views.py
+++ cinema/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count, F
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
 from cinema.models import (
     Genre,
@@ -9,7 +10,6 @@ from cinema.models import (
     MovieSession,
     Order,
 )
-
 from cinema.serializers import (
     GenreSerializer,
     ActorSerializer,
@@ -103,9 +103,17 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
         return queryset
 
 
+class SmallSetPagination(PageNumberPagination):
+    page_size = 10
+    page_query_param = "page_size"
+    max_page_size = 50
+
+
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.prefetch_related("tickets")
     serializer_class = OrderSerializer
+    pagination_class = SmallSetPagination
+
     def get_serializer_class(self):
         if self.action == "list":
             return OrderListSerializer
