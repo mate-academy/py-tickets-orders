79d4d85c365bc622bddbe02b734db83ab645bd10
tiway <stilon670@gmail.com>
2024-06-08 15:26:02 +0300

    Solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -105,8 +105,8 @@ class TicketSerializer(serializers.ModelSerializer):
                 {
                     "seat":
                         f"seat must be in range "
-                        f"[1, {attrs["movie_session"].cinema_hall.rows}], "
-                        f"not {attrs["seat"]}"
+                        f"[1, {attrs['movie_session'].cinema_hall.rows}], "
+                        f"not {attrs['seat']}"
                 }
             )
 
