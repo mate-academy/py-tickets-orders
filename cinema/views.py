dcf89be1fd08015fe2ce5d0918110f5e2d660054
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2024-07-09 12:55:19 +0300

    Create serializers and views for GET api/cinema/orders endpoint

--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,14 @@
 from rest_framework import viewsets
 
-from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession
+from cinema.models import (
+    Genre,
+    Actor,
+    CinemaHall,
+    Movie,
+    MovieSession,
+    Ticket,
+    Order,
+)
 
 from cinema.serializers import (
     GenreSerializer,
@@ -12,7 +20,11 @@ from cinema.serializers import (
     MovieDetailSerializer,
     MovieSessionDetailSerializer,
     MovieListSerializer,
+    TicketSerializer,
+    OrderSerializer,
+    OrderListSerializer,
 )
+from cinema.pagination import OrderPagination
 
 
 class GenreViewSet(viewsets.ModelViewSet):
@@ -56,3 +68,29 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
             return MovieSessionDetailSerializer
 
         return MovieSessionSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    pagination_class = OrderPagination
+
+    def get_queryset(self):
+        queryset = self.queryset.filter(user=self.request.user)
+        if self.action == "list":
+            queryset = queryset.prefetch_related("tickets__movie_session__cinema_hall")
+        return queryset
+
+    def get_serializer_class(self):
+        serializer = self.serializer_class
+        if self.action == "list":
+            serializer = OrderListSerializer
+        return serializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
