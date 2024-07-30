36ea646256b10da03a3802a545623c6264d3fd87
Sergii Semenets <serhey.semenets@gmail.com>
2024-07-30 15:40:39 +0300

    Solution fix pagination & flake8

--- cinema/views.py
+++ cinema/views.py
@@ -109,7 +109,7 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
 
 
 class OrderPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 5
     page_size_query_param = "page_size"
     max_page_size = 100
 
