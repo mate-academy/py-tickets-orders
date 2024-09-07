2c192167f4a34d0bcf82e88b4614fa5629023d21
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-09-07 18:33:26 +0300

    Add info about ticket in movie session

--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,4 @@
+from django.db.models import F, Count
 from rest_framework import viewsets
 
 from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession
@@ -64,7 +65,19 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
         if movie:
             queryset = MovieSession.objects.filter(id=movie)
 
-        if self.action in ("list", "retrieve"):
+        if self.action == "list":
+            queryset = (
+                queryset
+                .select_related("movie", "cinema_hall")
+                .annotate(
+                    tickets_available=(F("cinema_hall__rows")
+                                       * F("cinema_hall__seats_in_row")
+                                       - Count("tickets"))
+                )
+                .order_by("id")
+            )
+            return queryset
+        if self.action == "retrieve":
             return queryset.select_related("movie", "cinema_hall")
         return queryset
 
