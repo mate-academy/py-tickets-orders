139e120de1b47c809a023e6143d987953eb968cd
VlaBeh <166541377+VlaBeh@users.noreply.github.com>
2024-09-24 23:45:57 +0300

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -24,15 +24,22 @@ from cinema.serializers import (
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+
+
 class ActorViewSet(viewsets.ModelViewSet):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
+
+
 class CinemaHallViewSet(viewsets.ModelViewSet):
     queryset = CinemaHall.objects.all()
     serializer_class = CinemaHallSerializer
+
+
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
     def get_serializer_class(self):
         if self.action == "list":
             return MovieListSerializer
@@ -70,6 +77,7 @@ class MovieViewSet(viewsets.ModelViewSet):
 class MovieSessionViewSet(viewsets.ModelViewSet):
     queryset = MovieSession.objects.all()
     serializer_class = MovieSessionSerializer
+
     def get_serializer_class(self):
         if self.action == "list":
             return MovieSessionListSerializer
