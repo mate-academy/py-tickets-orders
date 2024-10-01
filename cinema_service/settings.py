00cc94e487687fe8e53586b26ab3d03500e5c11a
Alex <oleksandrtaranenko@gmail.com>
2024-10-01 18:45:06 +0300

    Solution fix

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -139,6 +139,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 REST_FRAMEWORK = {
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "DEFAULT_PAGINATION_CLASS":
+        "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 1
 }
