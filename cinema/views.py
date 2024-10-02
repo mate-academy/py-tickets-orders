742bc0f12b5abcc867dbc02430008add98ec3420
Halyna Rohatska <rohatskahr@gmail.com>
2024-10-02 20:52:29 +0300

    fixed

--- cinema/views.py
+++ cinema/views.py
@@ -102,9 +102,9 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
             .select_related()
             .annotate(
                 tickets_available=(
-                        F("cinema_hall__rows") *
-                        F("cinema_hall__seats_in_row") -
-                        Count("tickets")
+                        F("cinema_hall__rows")
+                        * F("cinema_hall__seats_in_row")
+                        - Count("tickets")
                 )
             )
         ).order_by("id")
