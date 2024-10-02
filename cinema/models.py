284ab8b9be3a3000a437494101841240d4f15311
Yaroslav Havryliuk <mordegear90@gmail.com>
2024-10-02 14:42:40 +0300

    sol_1

--- cinema/models.py
+++ cinema/models.py
@@ -1,6 +1,6 @@
+from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db import models
-from django.conf import settings
 
 
 class CinemaHall(models.Model):
@@ -84,23 +84,36 @@ class Ticket(models.Model):
     row = models.IntegerField()
     seat = models.IntegerField()
 
-    def clean(self):
-        for ticket_attr_value, ticket_attr_name, cinema_hall_attr_name in [
-            (self.row, "row", "rows"),
-            (self.seat, "seat", "seats_in_row"),
-        ]:
-            count_attrs = getattr(
-                self.movie_session.cinema_hall, cinema_hall_attr_name
+    @staticmethod
+    def validate_ticket_position(
+        row: int,
+        seat: int,
+        cinema_hall: CinemaHall
+    ) -> None:
+        max_rows = cinema_hall.rows
+        if not (1 <= row <= max_rows):
+            raise ValidationError(
+                {
+                    "row":
+                        f"Row number must be in available range: "
+                        f"(1, {max_rows})"
+                }
             )
-            if not (1 <= ticket_attr_value <= count_attrs):
-                raise ValidationError(
-                    {
-                        ticket_attr_name: f"{ticket_attr_name} "
-                        f"number must be in available range: "
-                        f"(1, {cinema_hall_attr_name}): "
-                        f"(1, {count_attrs})"
-                    }
-                )
+
+        max_seats_in_row = cinema_hall.seats_in_row
+        if not (1 <= seat <= max_seats_in_row):
+            raise ValidationError(
+                {
+                    "seat":
+                        f"Seat number must be in available range: "
+                        f"(1, {max_seats_in_row})"
+                }
+            )
+
+    def clean(self):
+        self.validate_ticket_position(
+            self.row, self.seat, self.movie_session.cinema_hall
+        )
 
     def save(
         self,
@@ -121,3 +134,4 @@ class Ticket(models.Model):
 
     class Meta:
         unique_together = ("movie_session", "row", "seat")
+        ordering = ("seat",)
