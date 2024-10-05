07226107680d9af829fd4f485c240c84cd0dc69a
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-10-05 16:07:36 +0200

    edit requirements.txt cinema/serializers.py

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,8 +1,16 @@
 from django.db import transaction
-from rest_framework import serializers
-from rest_framework.validators import UniqueTogetherValidator
 
-from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order, Ticket
+from rest_framework import serializers
+
+from cinema.models import (
+    Genre,
+    Actor,
+    CinemaHall,
+    Movie,
+    MovieSession,
+    Order,
+    Ticket
+)
 
 
 class TicketSerializer(serializers.ModelSerializer):
