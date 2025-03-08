774bb427306c219c0d64b3c97af110dea1724e3a
Artem <stilon670@gmail.com>
2025-03-08 13:27:16 +0200

    Added validate for OrderSerializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,6 +1,14 @@
 from rest_framework import serializers
 
-from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order, Ticket
+from cinema.models import (
+    Genre,
+    Actor,
+    CinemaHall,
+    Movie,
+    MovieSession,
+    Order,
+    Ticket
+)
 
 
 class GenreSerializer(serializers.ModelSerializer):
@@ -59,6 +67,7 @@ class MovieSessionListSerializer(MovieSessionSerializer):
     cinema_hall_capacity = serializers.IntegerField(
         source="cinema_hall.capacity", read_only=True
     )
+    tickets_available = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = MovieSession
@@ -68,17 +77,24 @@ class MovieSessionListSerializer(MovieSessionSerializer):
             "movie_title",
             "cinema_hall_name",
             "cinema_hall_capacity",
+            "tickets_available",
         )
+
 
 class TicketDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ticket
         fields = ("row", "seat")
 
+
 class MovieSessionDetailSerializer(MovieSessionSerializer):
     movie = MovieListSerializer(many=False, read_only=True)
     cinema_hall = CinemaHallSerializer(many=False, read_only=True)
-    taken_places = TicketDetailSerializer(many=True, read_only=True, source="tickets")
+    taken_places = TicketDetailSerializer(
+        many=True,
+        read_only=True,
+        source="tickets"
+    )
 
     class Meta:
         model = MovieSession
@@ -95,7 +111,6 @@ class TicketListSerializer(TicketSerializer):
     movie_session = MovieSessionListSerializer(many=False, read_only=True)
 
 
-
 class OrderSerializer(serializers.ModelSerializer):
     tickets = TicketSerializer(read_only=False, many=True)
 
@@ -104,11 +119,43 @@ class OrderSerializer(serializers.ModelSerializer):
         fields = ("id", "tickets", "created_at")
 
     def create(self, validated_data):
-        tickets_data = validated_data.pop('tickets')
+        tickets_data = validated_data.pop("tickets")
         order = Order.objects.create(**validated_data)
         for ticket_data in tickets_data:
             Ticket.objects.create(order=order, **ticket_data)
         return order
+
+    def validate(self, attrs):
+        for ticket in attrs["tickets"]:
+            cinema_hall = ticket["movie_session"].cinema_hall
+            row = ticket["row"]
+            seat = ticket["seat"]
+
+            self.validate_ticket_position(
+                value=row,
+                max_value=cinema_hall.rows,
+                name="Row"
+            )
+            self.validate_ticket_position(
+                value=seat,
+                max_value=cinema_hall.seats_in_row,
+                name="Seat"
+            )
+        return attrs
+
+    @staticmethod
+    def validate_ticket_position(
+            value: int,
+            max_value: int,
+            name: str
+    ):
+        if not (1 <= value <= max_value):
+            raise serializers.ValidationError(
+                {
+                    f"{name} must be in available range: "
+                    f"(1, {max_value})"
+                }
+            )
 
 
 class OrderListSerializer(OrderSerializer):
