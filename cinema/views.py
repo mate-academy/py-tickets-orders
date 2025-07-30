ea884415f461119a6eeb14d9b161eb19264aeb8f
Vitalii Ivanchenko <vanno1234567890@gmail.com>
2025-07-30 12:47:31 +0300

    solution

--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,7 @@
+from django.db.models import Count, F
 from rest_framework import viewsets
 
-from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession
+from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Ticket, Order
 
 from cinema.serializers import (
     GenreSerializer,
@@ -12,6 +13,10 @@ from cinema.serializers import (
     MovieDetailSerializer,
     MovieSessionDetailSerializer,
     MovieListSerializer,
+    TicketSerializer,
+    TicketListSerializer,
+    OrderListSerializer,
+    OrderSerializer
 )
 
 
@@ -43,6 +48,12 @@ class MovieViewSet(viewsets.ModelViewSet):
 
         return MovieSerializer
 
+    def get_queryset(self):
+        queryset = self.queryset
+        if self.action in ("list", "retrieve"):
+            queryset = queryset.prefetch_related("genres", "actors")
+        return queryset
+
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
     queryset = MovieSession.objects.all()
@@ -56,3 +67,61 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
             return MovieSessionDetailSerializer
 
         return MovieSessionSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if self.action in ("list", "retrieve"):
+            queryset = queryset.select_related(
+                "movie", "cinema_hall"
+            ).prefetch_related(
+                "tickets",
+                "movie__genres",
+                "movie__actors"
+            ).annotate(tickets_available=F("cinema_hall__seats_in_row") - Count("tickets"))
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return TicketListSerializer
+        return TicketSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if self.action == "list":
+            queryset = queryset.select_related(
+                "movie_session",
+                "order",
+                "movie_session__movie",
+                "movie_session__cinema_hall",
+                "order__user"
+            ).prefetch_related(
+                "movie_session__movie__genres",
+                "movie_session__movie__actors"
+            )
+            return queryset
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderListSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset.filter(user=self.request.user)
+        if self.action == "list":
+            queryset = queryset.select_related("user").prefetch_related(
+                "tickets",
+                "tickets__movie_session__movie",
+                "tickets__movie_session__cinema_hall",
+                "tickets__movie_session__movie__genres",
+                "tickets__movie_session__movie__actors"
+            )
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return OrderListSerializer
+        return OrderSerializer
