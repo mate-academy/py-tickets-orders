f97a390a7b85f69d3da5faa24f05f10e2a464465
Oleksii Drenov <leshadrenev@gmail.com>
2025-09-11 14:22:51 +0300

    add transaction.atomic to order write serializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -139,8 +139,9 @@ class OrderWriteSerializer(serializers.ModelSerializer):
         fields = ("id", "tickets", "created_at")
 
     def create(self, validated_data):
-        tickets_data = validated_data.pop("tickets")
-        order = Order.objects.create(**validated_data)
-        for ticket_data in tickets_data:
-            Ticket.objects.create(order=order, **ticket_data)
-        return order
+        with transaction.atomic():
+            tickets_data = validated_data.pop("tickets")
+            order = Order.objects.create(**validated_data)
+            for ticket_data in tickets_data:
+                Ticket.objects.create(order=order, **ticket_data)
+            return order
