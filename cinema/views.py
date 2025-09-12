50d03167ba7cfdc78eb079a55d46641ae5f777d4
Marcin <grzechnikmarcin@wp.pl>
2025-09-12 10:27:54 +0200

    py-tickets-orders solution restoring oryginal tests

--- cinema/views.py
+++ cinema/views.py
@@ -96,7 +96,7 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
 
 class OrderPagination(PageNumberPagination):
     page_size = 5
-    page_size_query_param = 'page_size'
+    page_size_query_param = "page_size"
     max_page_size = 100
 
 
