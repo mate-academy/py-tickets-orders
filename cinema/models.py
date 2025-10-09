8b7e84653e7fe358b985c759a83f45a49b127260
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-10-09 19:41:12 +0200

    Solution

--- cinema/models.py
+++ cinema/models.py
@@ -63,9 +63,8 @@ class MovieSession(models.Model):
 
 class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
-    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.created_at)
@@ -78,29 +77,32 @@ class Ticket(models.Model):
     movie_session = models.ForeignKey(
         MovieSession, on_delete=models.CASCADE, related_name="tickets"
     )
-    order = models.ForeignKey(
-        Order, on_delete=models.CASCADE, related_name="tickets"
-    )
+    order = models.ForeignKey(Order,
+                              on_delete=models.CASCADE,
+                              related_name="tickets")
     row = models.IntegerField()
     seat = models.IntegerField()
 
-    def clean(self):
-        for ticket_attr_value, ticket_attr_name, cinema_hall_attr_name in [
-            (self.row, "row", "rows"),
-            (self.seat, "seat", "seats_in_row"),
-        ]:
-            count_attrs = getattr(
-                self.movie_session.cinema_hall, cinema_hall_attr_name
+    @staticmethod
+    def validate_ticket(movie_session, row, seat):
+
+        cinema_hall = movie_session.cinema_hall
+
+        if not (1 <= row <= cinema_hall.rows):
+            raise ValidationError(
+                {"row": f"Row number must be in range (1, {cinema_hall.rows})"}
             )
-            if not (1 <= ticket_attr_value <= count_attrs):
-                raise ValidationError(
-                    {
-                        ticket_attr_name: f"{ticket_attr_name} "
-                        f"number must be in available range: "
-                        f"(1, {cinema_hall_attr_name}): "
-                        f"(1, {count_attrs})"
-                    }
-                )
+
+        if not (1 <= seat <= cinema_hall.seats_in_row):
+            raise ValidationError(
+                {
+                    "seat": f"Seat number must be in "
+                    f"range (1, {cinema_hall.seats_in_row})"
+                }
+            )
+
+    def clean(self):
+        Ticket.validate_ticket(self.movie_session, self.row, self.seat)
 
     def save(
         self,
@@ -109,15 +111,12 @@ class Ticket(models.Model):
         using=None,
         update_fields=None,
     ):
-        self.full_clean()
-        super(Ticket, self).save(
-            force_insert, force_update, using, update_fields
-        )
+        (super(Ticket, self)
+         .save(force_insert, force_update, using, update_fields))
 
     def __str__(self):
-        return (
-            f"{str(self.movie_session)} (row: {self.row}, seat: {self.seat})"
-        )
+        return (f"{str(self.movie_session)} "
+                f"(row: {self.row}, seat: {self.seat})")
 
     class Meta:
         unique_together = ("movie_session", "row", "seat")
