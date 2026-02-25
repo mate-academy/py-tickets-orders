770d9cb5bb96f0780a207687028a1638530a7f3c
Вадим Шевченко <grybefs@gmail.com>
2026-02-25 22:12:43 +0100

    Return taken places for movie session details endpoint

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -71,14 +71,19 @@ class MovieSessionListSerializer(MovieSessionSerializer):
             "cinema_hall_capacity",
         )
 
+class TicketTakenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = ("row", "seat")
 
 class MovieSessionDetailSerializer(MovieSessionSerializer):
     movie = MovieListSerializer(many=False, read_only=True)
     cinema_hall = CinemaHallSerializer(many=False, read_only=True)
+    taken_places = TicketTakenSerializer(source="tickets", many=True, read_only=True)
 
     class Meta:
         model = MovieSession
-        fields = ("id", "show_time", "movie", "cinema_hall")
+        fields = ("id", "show_time", "movie", "cinema_hall", "taken_places")
 
 
 class TicketSerializer(serializers.ModelSerializer):
