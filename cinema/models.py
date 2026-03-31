c4fdc23293ecf3ddaa64e3502362fc5acfca3c91
Sabina Gamidova <sabina.gamidova.dev@gmail.com>
2026-04-01 02:18:33 +0300

    Solution

--- cinema/models.py
+++ cinema/models.py
@@ -84,23 +84,32 @@ class Ticket(models.Model):
     row = models.IntegerField()
     seat = models.IntegerField()
 
+    @staticmethod
+    def validate_rows_seats(
+            rows: int,
+            seats_in_row: int,
+            row: int,
+            seat: int,
+            error_to_raise
+    ):
+        if not (1 <= row <= rows):
+            raise error_to_raise({
+                "row": f"Row must be between 1 and {rows}"
+            })
+
+        if not (1 <= seat <= seats_in_row):
+            raise error_to_raise({
+                "seat": f"Seat must be between 1 and {seats_in_row}"
+            })
+
     def clean(self):
-        for ticket_attr_value, ticket_attr_name, cinema_hall_attr_name in [
-            (self.row, "row", "rows"),
-            (self.seat, "seat", "seats_in_row"),
-        ]:
-            count_attrs = getattr(
-                self.movie_session.cinema_hall, cinema_hall_attr_name
-            )
-            if not (1 <= ticket_attr_value <= count_attrs):
-                raise ValidationError(
-                    {
-                        ticket_attr_name: f"{ticket_attr_name} "
-                        f"number must be in available range: "
-                        f"(1, {cinema_hall_attr_name}): "
-                        f"(1, {count_attrs})"
-                    }
-                )
+        Ticket.validate_rows_seats(
+            rows=self.movie_session.cinema_hall.rows,
+            seats_in_row=self.movie_session.cinema_hall.seats_in_row,
+            row=self.row,
+            seat=self.seat,
+            error_to_raise=ValidationError
+        )
 
     def save(
         self,
--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,17 @@
-from rest_framework import viewsets
+import datetime
 
-from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession
+from django.db.models import Prefetch, Count, F
+from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
+
+from cinema.models import (
+    Genre,
+    Actor,
+    CinemaHall,
+    Movie,
+    MovieSession,
+    Order, Ticket
+)
 
 from cinema.serializers import (
     GenreSerializer,
@@ -12,6 +23,8 @@ from cinema.serializers import (
     MovieDetailSerializer,
     MovieSessionDetailSerializer,
     MovieListSerializer,
+    OrderListSerializer,
+    OrderCreateSerializer,
 )
 
 
@@ -43,6 +56,26 @@ class MovieViewSet(viewsets.ModelViewSet):
 
         return MovieSerializer
 
+    def get_queryset(self):
+        queryset = Movie.objects.all()
+
+        genres = self.request.query_params.get("genres")
+        actors = self.request.query_params.get("actors")
+        title = self.request.query_params.get("title")
+
+        if genres:
+            genres_ids = [int(str_id) for str_id in genres.split(",")]
+            queryset = queryset.filter(genres__id__in=genres_ids)
+        if actors:
+            actors_ids = [int(str_id) for str_id in actors.split(",")]
+            queryset = queryset.filter(actors__id__in=actors_ids)
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+        return queryset.prefetch_related(
+            "genres",
+            "actors",
+        ).distinct()
+
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
     queryset = MovieSession.objects.all()
@@ -56,3 +89,74 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
             return MovieSessionDetailSerializer
 
         return MovieSessionSerializer
+
+    def get_queryset(self):
+        queryset = MovieSession.objects.select_related(
+            "movie",
+            "cinema_hall"
+        )
+        if self.action == "list":
+            queryset = (
+                queryset.annotate(
+                    tickets_available=(
+                        F("cinema_hall__rows")
+                        * F("cinema_hall__seats_in_row")
+                        - Count("tickets")
+                    )
+                )
+            )
+
+        date_data = self.request.query_params.get("date")
+        movie_id = self.request.query_params.get("movie")
+
+        if date_data:
+            queryset = queryset.filter(
+                show_time__date=date_data
+            )
+
+        if movie_id:
+            queryset = queryset.filter(
+                movie_id=int(movie_id)
+            )
+
+        return queryset.prefetch_related("tickets")
+
+
+class OrderSetPagination(PageNumberPagination):
+    page_size = 2
+    page_size_query_param = "page_size"
+    max_page_size = 100
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderListSerializer
+    pagination_class = OrderSetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return OrderCreateSerializer
+
+        return OrderListSerializer
+
+    def get_queryset(self):
+        queryset = Order.objects.all()
+
+        if self.action == "list":
+            return (
+                queryset
+                .filter(user=self.request.user)
+                .prefetch_related(
+                    Prefetch(
+                        "tickets",
+                        queryset=Ticket.objects.select_related(
+                            "movie_session__movie",
+                            "movie_session__cinema_hall"
+                        )
+                    )
+                )
+            )
+        return queryset
