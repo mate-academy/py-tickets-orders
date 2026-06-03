3180a961f0bab007d1c9f9e3aa6526798d2c2f3b
Dmytro Herman <dmytro.herman@zohomail.eu>
2026-06-03 18:11:01 +0300

    Disable pagination for all viewsets except OrderViewSet

--- cinema/views.py
+++ cinema/views.py
@@ -32,24 +32,28 @@ from cinema.serializers import (
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    pagination_class = None
 
 
 class ActorViewSet(viewsets.ModelViewSet):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
+    pagination_class = None
 
 
 class CinemaHallViewSet(viewsets.ModelViewSet):
     queryset = CinemaHall.objects.all()
     serializer_class = CinemaHallSerializer
+    pagination_class = None
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    pagination_class = None
 
     def get_queryset(self):
-        queryset = self.queryset
+        queryset = self.queryset.all()
         actors = self.request.query_params.get("actors")
         genres = self.request.query_params.get("genres")
         title = self.request.query_params.get("title")
@@ -83,9 +87,10 @@ class MovieViewSet(viewsets.ModelViewSet):
 class MovieSessionViewSet(viewsets.ModelViewSet):
     queryset = MovieSession.objects.all()
     serializer_class = MovieSessionSerializer
+    pagination_class = None
 
     def get_queryset(self):
-        queryset = self.queryset
+        queryset = self.queryset.all()
         date = self.request.query_params.get("date")
         movie = self.request.query_params.get("movie")
 
