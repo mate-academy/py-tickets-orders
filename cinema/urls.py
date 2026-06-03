410f71cd023cfdfbaaa6105afd10057471c03d00
Dmytro Herman <dmytro.herman@zohomail.eu>
2026-06-03 17:48:25 +0300

    Register OrderViewSet in cinema routes

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,13 +1,14 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework import routers
 
 from cinema.views import (
-    GenreViewSet,
     ActorViewSet,
     CinemaHallViewSet,
-    MovieViewSet,
+    GenreViewSet,
     MovieSessionViewSet,
-)
+    MovieViewSet,
+    OrderViewSet,
+    )
 
 router = routers.DefaultRouter()
 router.register("genres", GenreViewSet)
@@ -15,6 +16,7 @@ router.register("actors", ActorViewSet)
 router.register("cinema_halls", CinemaHallViewSet)
 router.register("movies", MovieViewSet)
 router.register("movie_sessions", MovieSessionViewSet)
+router.register("orders", OrderViewSet)
 
 urlpatterns = [path("", include(router.urls))]
 
