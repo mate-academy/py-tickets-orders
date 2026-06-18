7af5a37ff6dea10f9cf81480b4170f443a4064c5
Andriy <andrijbuluk@gmail.com>
2026-06-18 16:14:27 +0200

    Added: router for each page

--- cinema/urls.py
+++ cinema/urls.py
@@ -16,7 +16,7 @@ router.register("actors", ActorViewSet)
 router.register("cinema_halls", CinemaHallViewSet)
 router.register("movies", MovieViewSet)
 router.register("movie_sessions", MovieSessionViewSet)
-router.register("order", OrderViewSet)
+router.register("orders", OrderViewSet)
 
 urlpatterns = [path("", include(router.urls))]
 
