bdca31f83bc1b322cfb611024890d0f3391d33fd
Oleksandra Tymoshenko <aleksacat13@gmail.com>
2026-06-29 20:31:52 +0200

    first solution

--- cinema/models.py
+++ cinema/models.py
@@ -84,34 +84,60 @@ class Ticket(models.Model):
     row = models.IntegerField()
     seat = models.IntegerField()
 
-    def clean(self):
-        for ticket_attr_value, ticket_attr_name, cinema_hall_attr_name in [
-            (self.row, "row", "rows"),
-            (self.seat, "seat", "seats_in_row"),
-        ]:
-            count_attrs = getattr(
-                self.movie_session.cinema_hall, cinema_hall_attr_name
-            )
-            if not (1 <= ticket_attr_value <= count_attrs):
-                raise ValidationError(
-                    {
-                        ticket_attr_name: f"{ticket_attr_name} "
-                        f"number must be in available range: "
-                        f"(1, {cinema_hall_attr_name}): "
-                        f"(1, {count_attrs})"
-                    }
-                )
+    # def clean(self):
+    #     for ticket_attr_value, ticket_attr_name, cinema_hall_attr_name in [
+    #         (self.row, "row", "rows"),
+    #         (self.seat, "seat", "seats_in_row"),
+    #     ]:
+    #         count_attrs = getattr(
+    #             self.movie_session.cinema_hall, cinema_hall_attr_name
+    #         )
+    #         if not (1 <= ticket_attr_value <= count_attrs):
+    #             raise ValidationError(
+    #                 {
+    #                     ticket_attr_name: f"{ticket_attr_name} "
+    #                     f"number must be in available range: "
+    #                     f"(1, {cinema_hall_attr_name}): "
+    #                     f"(1, {count_attrs})"
+    #                 }
+    #             )
+    #
+    # def save(
+    #     self,
+    #     force_insert=False,
+    #     force_update=False,
+    #     using=None,
+    #     update_fields=None,
+    # ):
+    #     self.full_clean()
+    #     super(Ticket, self).save(
+    #         force_insert, force_update, using, update_fields
+    #     )
 
-    def save(
-        self,
-        force_insert=False,
-        force_update=False,
-        using=None,
-        update_fields=None,
+    @staticmethod
+    def validate_seat(
+            seat: int,
+            row: int,
+            seats_in_row: int,
+            rows: int,
+            error_to_raise
     ):
-        self.full_clean()
-        super(Ticket, self).save(
-            force_insert, force_update, using, update_fields
+        if not (1 <= seat <= seats_in_row):
+            raise error_to_raise({
+                "seat": f"{seat} must be in range [1, {seats_in_row}]"
+            })
+        if not (1 <= row <= rows):
+            raise error_to_raise({
+                "row": f"{row} must be in range [1, {rows}]"
+            })
+
+    def clean(self):
+        Ticket.validate_seat(
+            self.seat,
+            self.row,
+            self.movie_session.cinema_hall.seats_in_row,
+            self.movie_session.cinema_hall.rows,
+            ValidationError
         )
 
     def __str__(self):
--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,19 @@
 from rest_framework import viewsets
+from django.db.models import (
+    Q,
+    F,
+    Count,
+)
 
-from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession
+
+from cinema.models import (
+    Genre,
+    Actor,
+    CinemaHall,
+    Movie,
+    MovieSession,
+    Order,
+)
 
 from cinema.serializers import (
     GenreSerializer,
@@ -12,6 +25,8 @@ from cinema.serializers import (
     MovieDetailSerializer,
     MovieSessionDetailSerializer,
     MovieListSerializer,
+    OrderSerializer,
+    OrderListSerializer,
 )
 
 
@@ -43,6 +58,35 @@ class MovieViewSet(viewsets.ModelViewSet):
 
         return MovieSerializer
 
+    def get_queryset(self):
+        queryset = self.queryset
+
+        actors = self.request.query_params.get("actors")
+        genres = self.request.query_params.get("genres")
+        title = self.request.query_params.get("title")
+
+        if actors:
+            queryset = queryset.filter(
+                Q(actors__first_name__icontains=actors)
+                | Q(actors__last_name__icontains=actors)
+            )
+        if genres:
+            queryset = queryset.filter(
+                genres__name__icontains=genres
+            )
+        if title:
+            queryset = queryset.filter(
+                title__icontains=title
+            )
+
+        if self.action == "retrieve":
+            queryset = queryset.prefetch_related(
+                "actors",
+                "genres",
+            )
+
+        return queryset.distinct()
+
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
     queryset = MovieSession.objects.all()
@@ -56,3 +100,59 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
             return MovieSessionDetailSerializer
 
         return MovieSessionSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        date = self.request.query_params.get("date")
+        movie = self.request.query_params.get("movie")
+
+        if date:
+            queryset = queryset.filter(show_time__date=date)
+        if movie:
+            queryset = queryset.filter(movie_id=movie)
+
+        if self.action == "retrieve":
+            queryset = queryset.select_related(
+                "movie",
+                "cinema_hall"
+            )
+
+        if self.action == "list":
+            queryset = queryset.annotate(
+                tickets_available=(
+                    F("cinema_hall__rows")
+                    * F("cinema_hall__seats_in_row")
+                    - Count("tickets")
+                )
+            )
+
+        return queryset.distinct()
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def get_serializer_class(self):
+        if (
+            self.action == "list"
+            or self.action == "retrieve"
+        ):
+            return OrderListSerializer
+
+        return OrderSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        if self.action == "list":
+            queryset = queryset.prefetch_related(
+                "tickets",
+            )
+
+        queryset = queryset.filter(user=self.request.user)
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
