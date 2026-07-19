aa1bca2a81fbf31b3706b297d142752fd5fd9d1c
Pasha Liashenko <zedxi236@ukr.net>
2026-07-19 15:33:02 +0300

    Solution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -11,12 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import mimetypes
-
+from pathlib import Path
 mimetypes.add_type("application/javascript", ".js", True)
 mimetypes.add_type("text/css", ".css", True)
 
-
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
