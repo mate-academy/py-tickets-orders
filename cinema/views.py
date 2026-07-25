a1bb9175d41fd7f46f278e19eb88f385c74d6c68
SPMSPI <kislicin.artur@gmail.com>
2026-07-25 19:14:28 +0300

    read for recommend in flake8

--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,13 @@
 from rest_framework import viewsets, filters, pagination
 
-from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order, Ticket
+from cinema.models import (
+    Genre,
+    Actor,
+    CinemaHall,
+    Movie,
+    MovieSession,
+    Order,
+    Ticket)
 
 from cinema.serializers import (
     GenreSerializer,
--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -128,9 +128,9 @@ USE_I18N = True
 USE_TZ = True
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ],
 }
 
