cb9ea60ce63e980d8a5e5d48acf89ad3876ecba7
viloker <yarmolykvadym@gmail.com>
2026-08-14 23:32:53 +0200

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -108,7 +108,6 @@ class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
-
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user)
 
