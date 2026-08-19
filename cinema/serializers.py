be9aa77958dfc1a72c6c463f62e51a9396ffddd0
DimaPr7 <i.love.python.lt@gmail.com>
2026-08-19 19:10:37 +0300

    Fix flake8 formatting formatting for real

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -105,6 +105,7 @@ class TicketSerializer(serializers.ModelSerializer):
 class TicketListSerializer(TicketSerializer):
     movie_session = MovieSessionListSerializer(many=False, read_only=True)
 
+
 class OrderSerializer(serializers.ModelSerializer):
     tickets = TicketSerializer(many=True)
 
--- cinema/views.py
+++ cinema/views.py
@@ -85,10 +85,9 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
         if self.action == "list":
             queryset = (
                 queryset.select_related("cinema_hall", "movie")
-                .annotate(
-                    tickets_available=(
-                            F("cinema_hall__rows") * F("cinema_hall__seats_in_row")
-                            - Count("tickets")
+                .annotate( tickets_available=(
+                F("cinema_hall__rows") * F("cinema_hall__seats_in_row")
+                - Count("tickets")
                     )
                 )
             )
