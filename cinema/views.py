571eeb44f65b30682615747d791206c09367cd8d
Ростислав Дурягін <rostyslav.duriahin@lnu.edu.ua>
2026-08-19 18:09:41 +0200

    Fixed missing REST_FRAMEWORK pagination config in settings.py

--- cinema/views.py
+++ cinema/views.py
@@ -1,4 +1,3 @@
-from django.utils.dateparse import parse_date
 from rest_framework import viewsets, permissions
 from rest_framework.pagination import PageNumberPagination
 
@@ -20,21 +19,25 @@ from cinema.serializers import (
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    pagination_class = None
 
 
 class ActorViewSet(viewsets.ModelViewSet):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
+    pagination_class = None
 
 
 class CinemaHallViewSet(viewsets.ModelViewSet):
     queryset = CinemaHall.objects.all()
     serializer_class = CinemaHallSerializer
+    pagination_class = None
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    pagination_class = None
 
     def get_queryset(self):
         queryset = self.queryset
@@ -69,6 +72,7 @@ class MovieViewSet(viewsets.ModelViewSet):
 class MovieSessionViewSet(viewsets.ModelViewSet):
     queryset = MovieSession.objects.all()
     serializer_class = MovieSessionSerializer
+    pagination_class = None
 
     def get_queryset(self):
         queryset = MovieSession.objects.all()
@@ -94,15 +98,11 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
         return MovieSessionSerializer
 
 
-class OrderPagination(PageNumberPagination):
-    page_size = 10
-
-
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     permission_classes = (permissions.IsAuthenticated,)
-    pagination_class = OrderPagination
+    pagination_class = PageNumberPagination
 
     def get_queryset(self):
         return (
