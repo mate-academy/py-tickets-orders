354674d7badd190e75d0d2500cd802cc71f11248
Ваше Имя <klypcontacts@gmail.com>
2026-08-28 15:31:15 +0200

    Fix exact E231 and F541 errors

--- cinema/tests/test_movie_api.py
+++ cinema/tests/test_movie_api.py
@@ -47,7 +47,7 @@ class MovieApiTests(TestCase):
         )
         self.assertEqual(len(movies.data), 1)
         movies = self.client.get(
-            f"/api/cinema/movies/?genres={self.comedy.id},2,3"
+            f"/api/cinema/movies/?genres={self.comedy.id}, 2, 3"
         )
         self.assertEqual(len(movies.data), 1)
         movies = self.client.get("/api/cinema/movies/?genres=123213")
@@ -62,9 +62,9 @@ class MovieApiTests(TestCase):
         self.assertEqual(len(movies.data), 0)
 
     def test_get_movies_with_title_filtering(self):
-        movies = self.client.get(f"/api/cinema/movies/?title=ita")
+        movies = self.client.get("/api/cinema/movies/?title=ita")
         self.assertEqual(len(movies.data), 1)
-        movies = self.client.get(f"/api/cinema/movies/?title=ati")
+        movies = self.client.get("/api/cinema/movies/?title=ati")
         self.assertEqual(len(movies.data), 0)
 
     def test_post_movies(self):
