f2bfe1677ab799280309a95957891ee35f5f24ef
Danylo Pylypenko <p.prince.e777@gmail.com>
2026-09-03 18:51:38 +0300

    Add pagination in settings

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -48,6 +48,13 @@ INSTALLED_APPS = [
     "user",
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": (
+        "rest_framework.pagination.LimitOffsetPagination"
+    ),
+    "PAGE_SIZE": 10
+}
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
