63c2bd00c70f9c91be013f6846e2fa74cdefb7e1
Ваше Имя <klypcontacts@gmail.com>
2026-09-12 13:10:39 +0200

    Fix permissions for cinema views to allow authenticated read and admin write

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -1,10 +1,10 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
-class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
+class IsAdminOrIfAuthenticatedReadOnly(IsAuthenticatedOrReadOnly):
     def has_permission(self, request, view):
         return bool(
-            request.method in SAFE_METHODS
+            request.method in self.SAFE_METHODS
             and request.user
             and request.user.is_authenticated
             or request.user
